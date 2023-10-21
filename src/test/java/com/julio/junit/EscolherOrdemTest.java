1a5e6489cb3a7cd61efbf7920271b1dfd99f2408
Julio <juliobrito.tec@gmail.com>
2023-10-20 21:14:42 -0300

    add src/test/java/com/julio/junit/EscolherOrdemTest.java

---
+++ src/test/java/com/julio/junit/EscolherOrdemTest.java
@@ -0,0 +1,34 @@
+package com.julio.junit;
+
+import org.junit.jupiter.api.*;
+
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@TestMethodOrder(MethodOrderer.MethodName.class)
+//@TestMethodOrder(MethodOrderer.Random.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+public class EscolherOrdemTest {
+
+    @DisplayName("A")
+    @Test
+    void  validaFluxoA() {
+        Assertions.assertTrue(true);
+    }
+
+    @DisplayName("B")
+    @Test
+    void  validaFluxoB() {
+        Assertions.assertTrue(true);
+    }
+
+    @DisplayName("C")
+    @Test
+    void  validaFluxoC() {
+        Assertions.assertTrue(true);
+    }
+
+    @DisplayName("D")
+    @Test
+    void  validaFluxoD() {
+        Assertions.assertTrue(true);
+    }
+}
